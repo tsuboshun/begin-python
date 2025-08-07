3ede46f5d3323c21c3a51774a6813f4d18d3681b
tsuboshun <tsuboshun@gmail.com>
2025-08-07 21:51:45 +0900

    2025 fin

--- _static/custom.js
+++ _static/custom.js
@@ -1,20 +1,20 @@
-window.addEventListener("load", func=()=>{
+window.addEventListener("load", func = () => {
     const external_links = document.querySelectorAll("a.external");
-    for(let i=0; i<external_links.length; i++){
+    for (let i = 0; i < external_links.length; i++) {
         external_links[i].setAttribute("target", "_blank");
         external_links[i].setAttribute("rel", "noopener noreferrer");
     }
 });
 
-let text_close = "解答はこちら";
+let text_close = "解答例はこちら";
 let text_open = "閉じる";
 let color = '#f0f8ff';
 document.addEventListener('DOMContentLoaded', (event) => {
     const detailsElements = document.querySelectorAll('.toggle-details');
-    
+
     detailsElements.forEach(detailsElement => {
         const summaryElement = detailsElement.querySelector('.toggle-details__summary-text');
-        
+
         if (detailsElement.hasAttribute('open')) {
             summaryElement.innerText = text_open;
             detailsElement.style.backgroundColor = color;
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         } else {
             summaryElement.innerText = text_close;
         }
-        
+
         detailsElement.addEventListener('click', () => {
             setTimeout(() => {
                 if (detailsElement.hasAttribute('open')) {
